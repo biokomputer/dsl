68b8f410bf6772f6ed1d81681adad51a5ff7667f
Tom Sapletta <tom@sapletta.com>
2024-10-18 20:22:49 +0200

    aktualizacja dokumentacji i plikow paczki

--- setup.py
+++ setup.py
@@ -2,8 +2,9 @@ from setuptools import setup, find_packages
 
 setup(
     name='biocomp',
-    version='0.1',
-    packages=find_packages(),
+    version='0.1.1',
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
     install_requires=[
         'pyparsing==2.4.7',
         'matplotlib==3.4.3',
@@ -14,4 +15,21 @@ setup(
             'biocomp=biocomp:main',
         ],
     },
+    author="Tom Sapletta",
+    author_email="info@softreck.dev",
+    description="Biocomputing library",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://www.biokomputer.pl",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+    ],
 )
